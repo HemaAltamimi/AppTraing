0475cec4a2b26ae5b3b535139720bf76fc283dc2
itamimi <itamimi@itsolutions-now.com>
2022-12-17 15:01:44 +0300

    Finished Http Client from Anguler

--- API/Program.cs
+++ API/Program.cs
@@ -4,7 +4,6 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
 builder.Services.AddDbContext<DataContext>(opt =>
 {
@@ -14,15 +13,17 @@ builder.Services.AddDbContext<DataContext>(opt =>
  
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddCors();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
 
 app.MapControllers();
 
--- SPA/src/app/app.component.ts
+++ SPA/src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'SPA';
+  users : any;
+  constructor(private http:HttpClient) {
+
+  }
+  ngOnInit(): void {
+     this.http.get("http://localhost:5211/api/Users").subscribe(res => {
+      this.users =res;
+      console.log("next")
+     },
+     err => {console.log("error")},
+     () => {console.log("Complete")}
+     )
+  }
 }
